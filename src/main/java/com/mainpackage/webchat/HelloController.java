613408252726f356907ca38fbafd8cb7aae991b4
huifeng-kooboo <gisdoing@gmail.com>
2019-08-09 01:05:53 +0800

    实现简单的get请求返回Html界面
    实现请求

--- src/main/java/com/mainpackage/webchat/HelloController.java
+++ src/main/java/com/mainpackage/webchat/HelloController.java
@@ -1,16 +1,23 @@
 package com.mainpackage.webchat;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // 项目搭建参考：https://www.jianshu.com/p/d6b7a2806655
-@RestController
+
+//@RestController为@ResponseBody和@Controller的结合
+@Controller
 public class HelloController
 {
-    @RequestMapping("hello")
+    //value为url后缀  method为http请求方法，默认为get
+    @RequestMapping(value="/hello",method = RequestMethod.GET)
     public String hello()
     {
-        return  "Hello,World!" ;
+        return  "indexs";
     }
 }
+
 
--- src/main/java/com/mainpackage/webchat/WebchatApplication.java
+++ src/main/java/com/mainpackage/webchat/WebchatApplication.java
@@ -3,14 +3,11 @@ package com.mainpackage.webchat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
+/*SpringBoot 入口*/
 @SpringBootApplication
 public class WebchatApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebchatApplication.class, args);
     }
-
 }
 
