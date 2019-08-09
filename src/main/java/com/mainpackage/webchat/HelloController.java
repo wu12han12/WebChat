2abf04c1a3dfe51d5fac3d9f3b4d49a771b69153
huifeng-kooboo <gisdoing@gmail.com>
2019-08-10 01:48:29 +0800

    修改成get请求返回值.
    实现简单的get请求demo，实际上，需要重新创建文件夹，然后配数据库。后续完善，这个作为demo保存一个版本

--- src/main/java/com/mainpackage/webchat/HelloController.java
+++ src/main/java/com/mainpackage/webchat/HelloController.java
@@ -5,18 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.net.http.HttpRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // 项目搭建参考：https://www.jianshu.com/p/d6b7a2806655
 
 //@RestController为@ResponseBody和@Controller的结合
-@Controller
+//@Controller 能够实现返回html页面
+@RestController
 public class HelloController
 {
     //value为url后缀  method为http请求方法，默认为get
-    @RequestMapping(value="/hello",method = RequestMethod.GET)
-    public String hello()
+    @RequestMapping(value="/login",method = RequestMethod.GET)
+    //name 为get请求的请求名，需要与参数名相一致。
+    public String login(@RequestParam String name)
     {
-        return  "indexs";
+        return  name;
     }
 }
 
